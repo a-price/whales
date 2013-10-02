e956c8d57309db735a1c58b7dc885103c5a92449
Andrew Price <arprice@gatech.edu>
2013-10-01 23:13:35 -0400

    Experimenting with Harris corners to find fluke corners.

--- src/nodes/eigenwhales.cpp
+++ src/nodes/eigenwhales.cpp
@@ -63,8 +63,88 @@ inline VectorImage vectorize(const cv::Mat img)
 	return VectorImage((float*)img.data);
 }
 
+void findExtrema(const cv::Mat img)
+{
+	int scale = 1;
+	int delta = 0;
+	int ksize = 5;
+	int size = 3;
+	int ddepth = CV_32F;
+
+	cv::Mat smooth;
+	cv::Mat edge;
+	cv::Mat corners = cv::Mat::zeros(img.size(), CV_32FC1);
+	cv::Mat grad_x, grad_y;
+	cv::Mat orientation = cv::Mat::zeros(img.rows, img.cols, CV_32FC1);
+	cv::Mat magnitude = cv::Mat::zeros(img.rows, img.cols, CV_32FC1);
+	cv::Mat result = cv::Mat::zeros(img.rows, img.cols, CV_32FC3);
+
+	cv::GaussianBlur(img, smooth, cv::Size(size, size), 0, 0, cv::BORDER_DEFAULT);
+
+	/// Gradient X
+	cv::Sobel( smooth, grad_x, ddepth, 1, 0, ksize, scale, delta, cv::BORDER_DEFAULT );
+	/// Gradient Y
+	cv::Sobel( smooth, grad_y, ddepth, 0, 1, ksize, scale, delta, cv::BORDER_DEFAULT );
+
+	/// Detector parameters
+	int blockSize = 2;
+	int apertureSize = ksize;
+	double k = 0.04;
+
+	/// Detecting corners
+	cv::cornerHarris( smooth, corners, blockSize, apertureSize, k, cv::BORDER_DEFAULT );
+	/// Normalizing
+	cv::normalize( corners, corners, 0, 255, cv::NORM_MINMAX, CV_32FC1, cv::Mat() );
+	cv::convertScaleAbs( corners, corners );
+
+	smooth.convertTo(smooth, CV_8U);
+	cv::Canny(smooth, edge, 5, 5*3, 5);
+	cv::normalize(edge, edge, 0, 255, cv::NORM_MINMAX, CV_8UC1);
+
+	//cv::cvtColor(result, result, CV_BGR2HSV);
+	std::vector<cv::Mat> channels;
+	cv::split(result, channels);
+
+
+	for (int v = 0; v < img.rows; v++)
+	{
+		for (int u = 0; u < img.cols; u++)
+		{
+			int idx = (v * img.cols) + u;
+			float dx = grad_x.at<float>(idx);
+			float dy = grad_y.at<float>(idx);
+			orientation.at<float>(idx) = atan2(dy, dx);
+			magnitude.at<float>(idx) = sqrt(dx * dx + dy * dy);
+
+
+			channels[0].at<float>(idx) = (atan2(dy, dx) + M_PI) * 180.0/M_2_PI;
+			channels[1].at<float>(idx) = sqrt(dx * dx + dy * dy);
+			channels[2].at<float>(idx) = 255;
+			//cv::Vec3b pixel(((atan2(dy, dx) + M_PI) * 180.0/M_2_PI), sqrt(dx * dx + dy * dy), 255);
+
+			//result.at<cv::Vec3b>(idx) = pixel;
+		}
+	}
+
+	cv::normalize(channels[1], channels[1], 0, 255, cv::NORM_MINMAX, CV_32FC1);
+	channels[0].convertTo(channels[0], CV_8U);
+	channels[1].convertTo(channels[1], CV_8U);
+	channels[2] = corners; channels[2].convertTo(channels[2], CV_8U);
+	cv::merge(channels, result);
+
+	cv::cvtColor(result, result, CV_HSV2BGR);
+
+	cv::normalize(magnitude, magnitude, 0, 1.0, cv::NORM_MINMAX, CV_32FC1);
+	cv::normalize(orientation, orientation, 0, 1.0, cv::NORM_MINMAX, CV_32FC1);
+	std::cerr << "Hello." << std::endl;
+	//cv::normalize(result, result, 0, 180, cv::NORM_MINMAX, CV_8UC3);
+
+	cv::imshow("Hello", result);
+}
+
 int main(int argc, char** argv)
 {
+	cv::namedWindow("Hello", cv::WINDOW_NORMAL);
 	// Data storage
 	std::vector<cv::Mat> originalImages;
 
@@ -81,10 +161,17 @@ int main(int argc, char** argv)
 		temp /= 255.0;
 		// Equalize?
 		cv::normalize(temp, temp, 0, 1.0, cv::NORM_MINMAX, CV_32FC1);
+
+		if (imgIdx > 1 && imgIdx < 20)
+		{
+			findExtrema(temp);
+			cv::waitKey(0);
+		}
 		// Store
 		originalImages.push_back(temp);
 	}
 	//cv::imshow("img", originalImages[0]);
+	return 0;
 
 	std::cerr << "Number of Images: " << numFiles << std::endl;
 
